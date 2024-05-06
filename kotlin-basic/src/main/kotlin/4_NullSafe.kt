b0283c8ec3146fcbb59286ddd8988be6530dfd30
anstjaos <vv_eros@naver.com>
2024-05-06 12:37:36 +0900

    널 안정성 스터디

---
+++ kotlin-basic/src/main/kotlin/4_NullSafe.kt
@@ -0,0 +1,16 @@
+fun main() {
+//    val a : String = null
+//    var b : String = "aabbcc"
+//    b = null
+
+    // nullable type
+    var a : String? = null
+    println(a?.length)
+
+    val b : Int = if (a != null) a.length else 0
+    println(b)
+
+    // 엘비스 연산자
+    val c = a?.length ?: 0
+    println(c)
+}
